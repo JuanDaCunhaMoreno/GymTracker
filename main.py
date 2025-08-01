0f7b18a60e205f0fdfce53e3d58aa6a9c8eabf1b
Juan Moreno <juandacunhamoreno@gmail.com>
2025-08-01 17:50:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-from dados import calcular_volume_total, carregar_dados, agrupar_por_exercicio
-from graficos import grafico_linha_volume, grafico_barras_total, grafico_pizzas_exerciocios
+from dados import carregar_dados, calcular_volume_total
+from graficos import grafico_barras_total
 
 def main():
-    #Caminho do arquivo CSV
-    caminho = 'Treino.csv'
-    #Carregar dados
+    caminho = 'treino.csv'
+
+    # Carrega e processa os dados
     df = carregar_dados(caminho)
     df = calcular_volume_total(df)
-    df_agrupado = agrupar_por_exercicio(df)
-    #Gerar gráfico
-    grafico_linha_volume(df_agrupado)
+
+    # Mostra apenas o gráfico de barras
     grafico_barras_total(df)
-    grafico_pizzas_exerciocios(df)
-    #Encerrar o programa
-    print("Programa finalizado com sucesso.")
+
+    print("Gráfico de barras exibido com sucesso.")
 
 if __name__ == "__main__":
     main()
