efc33af8cedc667e1a4b61c83ddea3ebe9e13006
Juan Moreno <juandacunhamoreno@gmail.com>
2025-08-01 17:21:40 -0300

    Update graficos.py

--- graficos.py
+++ graficos.py
@@ -23,26 +23,26 @@ def grafico_linha_volume(df_agrupado):
 
     
 def grafico_barras_total(df):
-        #Plota um gráfico de barras com o volume total
-        #Agrupa dados por data e soma o volume total
-        volume_diario = df.groupby('data')['volume'].sum().reset_indnex()
+    #Plota um gráfico de barras com o volume total
+    #Agrupa dados por data e soma o volume total
+    volume_diario = df.groupby('data')['volume'].sum().reset_indnex()
 
-        plt.figure(figsize=(10, 6)) #Tamanho da figura
-        sns.barplot(x= 'data', y= 'volume', data= volume_diario, color= 'skyblue') #Plota as barras
+    plt.figure(figsize=(10, 6)) #Tamanho da figura
+    sns.barplot(x= 'data', y= 'volume', data= volume_diario, color= 'skyblue') #Plota as barras
 
-        plt.title('Volume Total de Treino por Dia')
-        plt.xlabel('data')
-        plt.ylabel('Volume Total')
-        plt.xticks(rotation= 45)
-        plt.tight_layout()
-        plt.show
+    plt.title('Volume Total de Treino por Dia')
+    plt.xlabel('data')
+    plt.ylabel('Volume Total')
+    plt.xticks(rotation= 45)
+    plt.tight_layout()
+    plt.show
 
 
- def grafico_pizzas_exerciocios(df):
-        #Plota gráfico de pizza
-        #Agrupa um gráfico de pizza e mostra a proporção de volume por exercício
-        volume_por_exercicio = df.groupby('exercicio')['volume'].sum()
-        plt.figure(figsize=( 8, 8))
-        plt.pie(volume_por_exercicio, labels= volume_por_exercicio.index, autopct= '%1.1f%%', startangle= 140) 
-        plt.axis('equal')
-        plt.show
+def grafico_pizzas_exerciocios(df):
+    #Plota gráfico de pizza
+    #Agrupa um gráfico de pizza e mostra a proporção de volume por exercício
+    volume_por_exercicio = df.groupby('exercicio')['volume'].sum()
+    plt.figure(figsize=( 8, 8))
+    plt.pie(volume_por_exercicio, labels= volume_por_exercicio.index, autopct= '%1.1f%%', startangle= 140) 
+    plt.axis('equal')
+    plt.show
