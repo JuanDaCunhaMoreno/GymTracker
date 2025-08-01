9de2062370accbc0ecb9c7062304527c45ea40c9
Juan Moreno <juandacunhamoreno@gmail.com>
2025-08-01 17:50:49 -0300

    Update graficos.py

--- graficos.py
+++ graficos.py
@@ -1,48 +1,23 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#Visual seaborn
-sns.set(style="whitegrid")
+def grafico_barras_total(df):
+    # Agrupa o volume total por data
+    df_total = df.groupby('data')['volume'].sum().reset_index()
 
+    # Estilo visual
+    sns.set(style="whitegrid")
 
-def grafico_linha_volume(df_agrupado):
-    #Plota um gráfico de linha mostrando evolução no exercício
-    plt.figure(figsize=(10, 6)) #Tamanho da figura
-    #Plota uma linha para cada exercício
-    for exercicio in df_agrupado['exercicio'].unique():
-        dados = df_agrupado[df_agrupado['exercicio'] == exercicio]
-        plt.plot(dados['data'], dados['volume'], marker = 'o', label = exercicio)
+    # Criação do gráfico de barras
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x='data', y='volume', data=df_total, palette='Blues_d')
 
-    plt.title("Volume de Treino por Exercício")
+    # Títulos e rótulos
+    plt.title('Volume Total de Treino por Dia')
     plt.xlabel('Data')
     plt.ylabel('Volume Total')
-    plt.legend(title= 'Exercicio')
-    plt.xticks(rotation = 45) #Rotaciona datas
+    plt.xticks(rotation=45)
+
+    # Exibe o gráfico
     plt.tight_layout()
-    plt.show() #Exibe o gráfico
-
-    
-def grafico_barras_total(df):
-    #Plota um gráfico de barras com o volume total
-    #Agrupa dados por data e soma o volume total
-    volume_diario = df.groupby('data')['volume'].sum().reset_indnex()
-
-    plt.figure(figsize=(10, 6)) #Tamanho da figura
-    sns.barplot(x= 'data', y= 'volume', data= volume_diario, color= 'skyblue') #Plota as barras
-
-    plt.title('Volume Total de Treino por Dia')
-    plt.xlabel('data')
-    plt.ylabel('Volume Total')
-    plt.xticks(rotation= 45)
-    plt.tight_layout()
-    plt.show
-
-
-def grafico_pizzas_exerciocios(df):
-    #Plota gráfico de pizza
-    #Agrupa um gráfico de pizza e mostra a proporção de volume por exercício
-    volume_por_exercicio = df.groupby('exercicio')['volume'].sum()
-    plt.figure(figsize=( 8, 8))
-    plt.pie(volume_por_exercicio, labels= volume_por_exercicio.index, autopct= '%1.1f%%', startangle= 140) 
-    plt.axis('equal')
-    plt.show
+    plt.show()
